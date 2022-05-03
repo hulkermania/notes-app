459bc4dbf5a7e7b179fb1e8349f1e22ca7b81e99
hulkermania <whdgkr6854@gmail.com>
2022-05-03 11:08:16 +0900

    서버리스 메모 앱-2

---
+++ frontend/src/containers/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p className="text-muted">심플한 메모 앱</p>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/containers/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound text-center">
+      <h3>현재 요청하신 페이지는 존재하지 않습니다.</h3>
+    </div>
+  );
+}
